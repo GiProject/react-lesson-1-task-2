e967043f2257700a4086cb80aa64d1fb9b453257
Evgeniy Kulinichev <gekin010@ya.ru>
2023-05-02 22:09:55 +0300

    task 2

--- src/components/ShopItemFunc.js
+++ src/components/ShopItemFunc.js
@@ -1,16 +1,25 @@
-function ShopItemFunc ({ item }) {
-    return <div class="main-content">
-        <h2>{item.brand}</h2>
-        <h1>{item.title}</h1>
-        <h3>{item.description}</h3>
-        <div class="description">{item.descriptionFull}</div>
-        <div class="highlight-window mobile"><div class="highlight-overlay"></div></div>
-        <div class="divider"></div>
-        <div class="purchase-info">
-        <div class="price">{item.currency}{item.price}</div>
-            <button>Добавить в корзину</button>
+import React from "react";
+
+class ShopItemFunc extends React.Component {
+    render() {
+        const { item } = this.props;
+
+        return 
+        <div class="main-content">
+                <h2>{item.brand}</h2>
+                <h1>{item.title}</h1>
+                <h3>{item.description}</h3>
+                <div className="description">{item.descriptionFull}</div>
+                <div className="highlight-window mobile">
+                    <div className="highlight-overlay"></div>
+                </div>
+                <div className="divider"></div>
+                <div className="purchase-info">
+                <div className="price">{item.currency}{item.price}</div>
+                    <button>Добавить в корзину</button>
+                </div>
         </div>
-  </div>
+    }
 }
 
 export default ShopItemFunc;
